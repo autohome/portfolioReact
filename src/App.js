f0c51a560392f3eba8d417ad56444d202e51a83f
autohome <joshdadbin@gmail.com>
2023-05-09 10:49:53 -0700

    file structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import BucketList from './components/BucketList';
+
+function App() {
+  return (
+    <div className="">
+      {/* TODO - update everything with BucketList */}
+      <BucketList />
+    </div>
+  );
+}
+
+export default App;
